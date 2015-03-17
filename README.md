f20f8b6aede62627063d9dd4bfe536440cf76c04
Bojan Djurkovic <dbojan@gmail.com>
2015-03-16 21:08:55 -0300

    json schema dereference utility

---
+++ README.md
@@ -0,0 +1,112 @@
+# json-schema-deref
+
+Dereference JSON pointers in a JSON schemas with their true resolved values.
+Basically a lighter, synchronous version of [json-schema-deref](https://github.com/bojand/json-schema-deref) but omits web references and
+custom loaders.
+
+## Installation
+
+`npm install json-schema-deref-sync`
+
+## Overview
+
+Let's say you have the following JSON Schema:
+
+```json
+{
+  "description": "Just some JSON schema.",
+  "title": "Basic Widget",
+  "type": "object",
+  "definitions": {
+    "id": {
+      "description": "unique identifier",
+      "type": "string",
+      "minLength": 1,
+      "readOnly": true
+    }
+  },
+  "properties": {
+  "id": {
+    "$ref": "#/definitions/id"
+  },
+  "foo": {
+    "$ref": "http://www.mysite.com/myschema.json#/definitions/foo"
+  },
+  "bar": {
+    "$ref": "bar.json"
+  }
+}
+```
+
+Sometimes you just want that schema to be fully expanded, with `$ref`'s being their (true) resolved values:
+
+```json
+{
+  "description": "Just some JSON schema.",
+  "title": "Basic Widget",
+  "type": "object",
+  "definitions": {
+    "id": {
+      "description": "unique identifier",
+      "type": "string",
+      "minLength": 1,
+      "readOnly": true
+    }
+  },
+  "properties": {
+    "id": {
+      "description": "unique identifier",
+      "type": "string",
+      "minLength": 1,
+      "readOnly": true
+    },
+    "foo": {
+      "description": "foo property",
+      "readOnly": true,
+      "type": "number"
+    },
+    "bar": {
+      "description": "bar property",
+      "type": "boolean"
+    }
+  }
+}
+```
+
+This utility lets you do that:
+
+
+```js
+var deref = require('json-schema-deref');
+var myschema = require('schema.json');
+
+var fullSchema = deref(myschema);
+```
+
+## API
+
+### deref(schema, options)
+
+Dereferences `$ref`'s in json schema to actual resolved values. Supports local, and file refs.
+
+Parameters:
+
+##### `schema`
+The input JSON schema
+
+##### `options`
+
+`baseFolder` - the base folder to get relative path files from. Default is `process.cwd()`
+
+### deref.getRefPathValue(schema, refPath)
+
+Gets the "local" ref value given the path.
+
+`schema` - the (root) json schema to search
+
+`refPath` - string ref path to get within the schema. Ex. `#/definitions/id`
+
+```js
+var localValue = deref.getRefPathValue(myschema, '#/definitions/foo');
+console.dir(localValue);
+```
---
+++ test/index.js
@@ -0,0 +1,202 @@
+describe('json-schema-deref', function () {
+  var expect = require('chai').expect;
+  var deref = require('../lib');
+  var path = require('path');
+  var fsx = require('fs.extra');
+  var async = require('async');
+
+  var tempFolder = '/var/tmp/json-deref-schema-tests/';
+  before(function (done) {
+    var srcfiles = ['id.json', 'foo.json', 'bar.json'];
+    fsx.mkdirpSync(tempFolder);
+    async.eachSeries(srcfiles, function (filePath, cb) {
+      var srcFile = path.resolve(path.join(__dirname, './schemas', filePath));
+      var desFile = path.join('/var/tmp/json-deref-schema-tests/', filePath);
+      fsx.copy(srcFile, desFile, cb);
+    }, done);
+  });
+
+  after(function (done) {
+    fsx.rmrf(tempFolder, done);
+  });
+
+  describe('deref', function () {
+    it('should work with basic schema', function () {
+      var basicSchema = require('./schemas/basic');
+
+      var schema = deref(basicSchema);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(basicSchema);
+    });
+
+    it('should work with basic local refs', function () {
+      var input = require('./schemas/localrefs');
+      var expected = require('./schemas/localrefs.expected.json');
+
+      var schema = deref(input);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with basic file refs and relative baseFolder', function () {
+      var input = require('./schemas/basicfileref');
+      var expected = require('./schemas/basic');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with basic file refs and absolute + relative baseFolder', function () {
+      var input = require('./schemas/basicfileref');
+      var expected = require('./schemas/basic');
+
+      var schema = deref(input, {baseFolder: __dirname + '/./schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with basic file refs and absolute baseFolder', function () {
+      var input = require('./schemas/basicfileref');
+      var expected = require('./schemas/basic');
+
+      var schema = deref(input, {baseFolder: path.join(__dirname, 'schemas')});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with local and file refs', function () {
+      var input = require('./schemas/localandfilerefs');
+      var expected = require('./schemas/localrefs.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with absolute files', function () {
+      var input = require('./schemas/filerefs');
+      var expected = require('./schemas/basic.json'); // same expected output
+
+      var schema = deref(input);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with simple web refs', function () {
+      var input = require('./schemas/webrefs');
+      var expected = require('./schemas/webrefs.expected.json'); // same expected output
+
+      var schema = deref(input);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with web and local mixed refs', function () {
+      var input = require('./schemas/webwithlocal');
+      var expected = require('./schemas/webwithlocal.expected.json');
+
+      var schema = deref(input);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with web refs with json pointers', function () {
+      var input = require('./schemas/webrefswithpointer');
+      var expected = require('./schemas/webrefswithpointer.expected.json');
+
+      var schema = deref(input);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with file refs with json pointers', function () {
+      var input = require('./schemas/filerefswithpointer');
+      var expected = require('./schemas/filerefswithpointer.expected.json'); // same expected output
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with nested json pointers', function () {
+      var input = require('./schemas/api.props.json');
+      var expected = require('./schemas/api.props.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with nested json pointers to files and links ref to files', function () {
+      var input = require('./schemas/api.linksref.json');
+      var expected = require('./schemas/api.linksref.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with nested json pointers to files with redirect to file in an array', function () {
+      var input = require('./schemas/arrayfileref.json');
+      var expected = require('./schemas/arrayfileref.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with deep links', function () {
+      var input = require('./schemas/apideeplink.json');
+      var expected = require('./schemas/apideeplink.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with deep nested ref links', function () {
+      var input = require('./schemas/apinestedrefs.json');
+      var expected = require('./schemas/apinestedrefs.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with custom types', function () {
+      var input = require('./schemas/customtype.json');
+      var expected = require('./schemas/customtype.expected.json');
+
+      var options = {
+        baseFolder: './test/schemas'
+      };
+
+      var schema = deref(input, options);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work unknown type 2', function () {
+      var input = require('./schemas/customunknown.json');
+      var expected = require('./schemas/customunknown.expected.json');
+
+      var options = {
+        baseFolder: './test/schemas'
+      };
+
+      var schema = deref(input, options);
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+
+    it('should work with missing properties', function () {
+      var input = require('./schemas/missing.json');
+      var expected = require('./schemas/missing.expected.json');
+
+      var schema = deref(input, {baseFolder: './test/schemas'});
+      expect(schema).to.be.ok;
+      expect(schema).to.deep.equal(expected);
+    });
+  });
+});
