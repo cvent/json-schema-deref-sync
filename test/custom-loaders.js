b95ffd0e195b77d2a4615dffbebfc591ea5e39c3
kobi.carmeli <kobi.carmeli@zooz.com>
2019-09-14 13:56:45 +0300

    simplify test file loader

--- test/custom-loaders.js
+++ test/custom-loaders.js
@@ -17,32 +17,13 @@ const file = function (refValue, options) {
   }
 
   const filePath = getRefFilePath(refPath)
-  const filePathLowerCase = filePath.toLowerCase()
 
   let newValue
   try {
     var data = fs.readFileSync(filePath, 'utf8')
-    if (filePathLowerCase.endsWith('.json')) {
-      newValue = parseJson(data) || parseYaml(data)
-    } else if (filePathLowerCase.endsWith('.yml') || filePathLowerCase.endsWith('.yaml')) {
-      newValue = parseYaml(data)
-    } else {
-      newValue = parseJson(data) || parseYaml(data)
-    }
+    newValue = jsyaml.load(data)
   } catch (e) {}
 
   return newValue
 }
 module.exports.file = file
-
-function parseJson(data) {
-  try {
-    return JSON.parse(data)
-  } catch (e) {}
-}
-
-function parseYaml(data) {
-  try {
-    return jsyaml.load(data)
-  } catch (e) {}
-}
